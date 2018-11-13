a8a56816e42ce2cfc4012b71c03d7af3f1692259
Oleg <garrbuzz@gmail.com>
2018-11-13 22:43:21 +0200

    wow

---
+++ js/calculator.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+	var numbOf = document.querySelector('#numberOfOffices');
+	var numbServ = document.querySelector('#numberOfServers');
+	var numbComp = document.querySelector('#numberOfComputers');
+	var atsType = document.querySelectorAll('input[name="ats"]');
+	numbOf.value=1;
+	numbServ.value = 0;
+	numbComp.value = 1;
+	atsType[0].checked=true;
+	atsType[0].value=not;
+
+	
+
+}
