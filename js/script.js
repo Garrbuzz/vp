e1c150055a843cc5b1812992182a05c5867f8301
Oleg <garrbuzz@gmail.com>
2018-11-20 23:07:24 +0200

    +incident

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ function modalWindow () {
 	this.modal=document.querySelector('.modal');
 	this.overlay=document.querySelector('.overlay');
 	var popup = this;
-	this.open=function(content){
+	popup.open=function(content){
 		popup.modal.innerHTML = content;
 		popup.overlay.classList.add('visible');
 		popup.modal.classList.add('visible');
 	}
-	this.close=function(){
+	popup.close=function(){
 		popup.overlay.classList.remove('visible');
 		popup.modal.classList.remove('visible');
 	}
-	this.overlay.onclick = popup.close;
+	popup.overlay.onclick = popup.close;
 
 }
